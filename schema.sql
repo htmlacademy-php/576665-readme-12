7284c313ae0df50e5f3bf377324f36d74bfd76d0
Nadiia Derevianchenko <nadiya.derevyanchenko@gmail.com>
2020-06-18 21:42:53 +0300

    update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,71 +1,73 @@
 CREATE DATABASE readme
 DEFAULT CHARACTER SET utf8
 DEFAULT COLLATE utf8_general_ci;
+
 USE readme;
 
 CREATE TABLE users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_registered TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    user_email VARCHAR (320) UNIQUE,
-    user_login VARCHAR (60) UNIQUE,
-    user_password VARCHAR(60),
-    user_pic CHAR
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    registered TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    email VARCHAR (320) UNIQUE,
+    login VARCHAR (60) UNIQUE,
+    password VARCHAR(60),
+    picture CHAR
 );
 
 CREATE TABLE posts (
-    post_id INT AUTO_INCREMENT PRIMARY KEY,
-    post_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    post_title VARCHAR(60),
-    post_content TEXT,
-    post_auth_quote VARCHAR(60),
-    post_img CHAR,
-    post_video CHAR,
-    post_link CHAR,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    title VARCHAR(60),
+    content TEXT,
+    author_quote VARCHAR(60),
+    img CHAR,
+    video CHAR,
+    link CHAR,
     view_count INT,
-    post_auth INT REFERENCES users (user_id),
-    post_type INT REFERENCES post_types (post_type_id),
-    post_teg INT REFERENCES tags (tag_id)
+    user_id INT REFERENCES users (id),
+    post_type_id INT REFERENCES post_types (id),
+    tag_id INT REFERENCES tags (id)
 );
 
 CREATE TABLE comments (
-    comment_id INT AUTO_INCREMENT PRIMARY KEY,
-    comment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    comment_content TEXT,
-    comment_auth INT REFERENCES users (user_id),
-    comment_post INT REFERENCES posts (post_id)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    content TEXT,
+    user_id INT REFERENCES users (id),
+    post_id INT REFERENCES posts (id)
 );
 
 CREATE TABLE likes (
-    like_id INT AUTO_INCREMENT PRIMARY KEY,
-    like_user INT REFERENCES users (user_id),
-    like_post INT REFERENCES posts (post_id)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT REFERENCES users (id),
+    post_id INT REFERENCES posts (id)
 );
 
-CREATE TABLE subs (
-    sub_id INT AUTO_INCREMENT PRIMARY KEY,
-    sub_user INT REFERENCES users (user_id),
-    sub_post INT REFERENCES posts (post_id)
+CREATE TABLE subscriptions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT REFERENCES users (id),
+    post_id INT REFERENCES posts (id)
 );
 
 CREATE TABLE massages (
-    massage_id INT AUTO_INCREMENT PRIMARY KEY,
-    massage_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    massage_content TEXT,
-    massage_sender INT REFERENCES users (user_id),
-    massage_addressee INT REFERENCES users (user_id)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    content TEXT,
+    user_sender_id INT REFERENCES users (id),
+    user_recipient_id INT REFERENCES users (id)
 );
 
 CREATE TABLE tags (
-    tag_id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     tag VARCHAR(30) UNIQUE
 );
 
 CREATE TABLE post_types (
-    post_type_id INT AUTO_INCREMENT PRIMARY KEY,
-    post_type_name VARCHAR(30) UNIQUE,
-    post_type_class VARCHAR(10) UNIQUE
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) UNIQUE,
+    class VARCHAR(10) UNIQUE
 );
 
+CREATE INDEX view_count ON posts (view_count);
 
-
+CREATE INDEX date ON posts (date);
 
