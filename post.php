c51946ea10af21186b12211affb0b41951226159
Nadiia Derevianchenko <nadiya.derevyanchenko@gmail.com>
2020-07-09 22:07:06 +0300

    refactor: use prepared statements

--- post.php
+++ post.php
@@ -5,45 +5,46 @@ require_once 'helpers.php';
 
 if (isset($_GET['post_id'])) {
     $param_id = filter_input(INPUT_GET, 'post_id');
-}
+    $post = '';
 
-if (empty($link)) {
-    print ('error' . mysqli_connect_error());
-} else {
     $sql = 'SELECT * , users.id, post_types.id FROM posts'
         . ' JOIN users ON posts.user_id = users.id'
         . ' JOIN post_types ON posts.post_type_id = post_types.id'
-        . ' WHERE post_id =' . $param_id;
+        . ' WHERE post_id =' . ' ?';
 
-    $result = mysqli_query($link, $sql);
+    $stmt = db_get_prepare_stmt($link, $sql, [
+        'i' => $param_id
+    ]);
 
-    if ($result) {
-        $post = mysqli_fetch_assoc($result);
-    }
+    mysqli_stmt_execute($stmt);
 
-    $sql = 'SELECT * FROM subscriptions'
-        . ' WHERE subscriptions.author_id = ' . $post['user_id'];
+    $res = mysqli_stmt_get_result($stmt);
 
-    $result = mysqli_query($link, $sql);
-
-    if ($result) {
-        $subscriptions_count = mysqli_num_rows($result);
-    }
-
-    $sql = 'SELECT * FROM posts'
-        . ' WHERE posts.user_id = ' . $post['user_id'];
-
-    $result = mysqli_query($link, $sql);
-
-    if($result) {
-        $posts_count = mysqli_num_rows($result);
-    }
+    $post = mysqli_fetch_assoc($res);
 }
 
 if (empty($post)) {
     header("HTTP/1.0 404 Not Found");
-    $error_msg = 'Не удалось выполнить запрос: ' . mysqli_error($link);
+    $error_msg = 'PAGE NOT FOUND: ' . mysqli_error($link);
     print ($error_msg);
+}
+
+$sql = 'SELECT * FROM subscriptions'
+    . ' WHERE subscriptions.author_id = ' . $post['user_id'];
+
+$result = mysqli_query($link, $sql);
+
+if ($result) {
+    $subscriptions_count = mysqli_num_rows($result);
+}
+
+$sql = 'SELECT * FROM posts'
+    . ' WHERE posts.user_id = ' . $post['user_id'];
+
+$result = mysqli_query($link, $sql);
+
+if ($result) {
+    $posts_count = mysqli_num_rows($result);
 }
 
 $post_content = include_template("post-{$post['class']}.php", [
