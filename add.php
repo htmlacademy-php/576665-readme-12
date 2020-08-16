14bc5a5ebe43fa663164c7a23ab76eb9e81fce0e
Nadiia Derevianchenko <nadiya.derevyanchenko@gmail.com>
2020-08-16 21:26:45 +0300

    fix: add existing session condition

--- add.php
+++ add.php
@@ -4,6 +4,11 @@ require_once 'init.php';
 require_once 'helpers.php';
 require_once 'functions.php';
 
+if (!isset($_SESION['user'])) {
+    header("Location: /index.php");
+    exit();
+}
+
 $sql = 'SELECT * from post_types';
 $result = mysqli_query($link, $sql);
 if (!$result) {
