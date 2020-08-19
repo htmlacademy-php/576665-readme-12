cfb57326e0893ef938cc3c3a2e3c2b2bcec25d49
Nadiiy <nadiya.derevyanchenko@gmail.com>
2020-08-19 16:51:17 +0300

    add: get_post_tags() function

--- search.php
+++ search.php
@@ -14,6 +14,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
     $search_query = trim($_GET['q']) ?? '';
 
+    $search_sql = 'SELECT * FROM posts '
+        . 'JOIN users ON posts.user_id = users.id '
+        . 'JOIN post_types ON posts.post_type_id = post_types.id ';
+
     if (substr($search_query, 0, 1) === '#') {
         $search_tag = substr($search_query, 1);
         $sql = "SELECT id FROM tags WHERE tag = ?";
@@ -24,34 +28,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         if (!$result) {
             exit ('error' . mysqli_error($link));
         }
-        $search_tags_id = mysqli_fetch_row($result);
-        $search_tags_id = implode('', $search_tags_id);
-        var_dump($search_tags_id);
-
-        $sql = 'SELECT post_id FROM post_tag '
-            . 'WHERE tag_id = ' . $search_tags_id;
+        $search_tag_id = mysqli_fetch_row($result);
+        $search_tag_id = implode('', $search_tag_id);
+        $sql = "SELECT post_id FROM post_tag WHERE tag_id = {$search_tag_id}";
         $result = mysqli_query($link, $sql);
         $post_id = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-        var_dump($post_id);
         $post_id = implode(', ', array_column($post_id, 'post_id'));
-        print $post_id;
-
-        $sql = 'SELECT * FROM posts WHERE post_id = (' . $post_id . ')';
-        print $sql;
-        $result = mysqli_query($link, $sql);
+        $search_sql .= "WHERE post_id IN ({$post_id})";
+        $result = mysqli_query($link, $search_sql);
         if (!$result) {
             exit ('error' . mysqli_error($link));
         }
+
         $posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
     } else {
 
-        $sql = 'SELECT * FROM posts '
-            . 'JOIN users ON posts.user_id = users.id '
-            . 'JOIN post_types ON posts.post_type_id = post_types.id '
-            . 'WHERE MATCH(title, content) AGAINST(?) ';
-        $stmt = db_get_prepare_stmt($link, $sql, [$search_query]);
+        $search_sql .= 'WHERE MATCH(title, content) AGAINST(?) ';
+        $stmt = db_get_prepare_stmt($link, $search_sql, [$search_query]);
 
         mysqli_stmt_execute($stmt);
 
@@ -62,11 +56,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         }
 
         $posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    }
+
+    foreach ($posts as $key => $post) {
+        $posts[$key]['tags'] = get_post_tags($post['post_id'], $link);
     }
 }
 
 $page_content = include_template('search-results.php', [
-    'posts' => $posts,
+    'posts' => $posts ?? '',
     'search_query' => $search_query
 ]);
 
--- templates/search-results.php
+++ templates/search-results.php
@@ -106,36 +106,38 @@
                                     </div>
                                 <?php endswitch ?>
                             </div>
-                            <footer class="post__footer post__indicators">
-                                <div class="post__buttons">
-                                    <a class="post__indicator post__indicator--likes button" href="#" title="Лайк">
-                                        <svg class="post__indicator-icon" width="20" height="17">
-                                            <use xlink:href="#icon-heart"></use>
-                                        </svg>
-                                        <svg class="post__indicator-icon post__indicator-icon--like-active" width="20"
-                                             height="17">
-                                            <use xlink:href="#icon-heart-active"></use>
-                                        </svg>
-                                        <span>250</span>
-                                        <span class="visually-hidden">количество лайков</span>
-                                    </a>
-                                    <a class="post__indicator post__indicator--comments button" href="#"
-                                       title="Комментарии">
-                                        <svg class="post__indicator-icon" width="19" height="17">
-                                            <use xlink:href="#icon-comment"></use>
-                                        </svg>
-                                        <span>25</span>
-                                        <span class="visually-hidden">количество комментариев</span>
-                                    </a>
+                            <footer class="post__footer">
+                                <div class="post__indicators">
+                                    <div class="post__buttons">
+                                        <a class="post__indicator post__indicator--likes button" href="#" title="Лайк">
+                                            <svg class="post__indicator-icon" width="20" height="17">
+                                                <use xlink:href="#icon-heart"></use>
+                                            </svg>
+                                            <svg class="post__indicator-icon post__indicator-icon--like-active"
+                                                 width="20"
+                                                 height="17">
+                                                <use xlink:href="#icon-heart-active"></use>
+                                            </svg>
+                                            <span>250</span>
+                                            <span class="visually-hidden">количество лайков</span>
+                                        </a>
+                                        <a class="post__indicator post__indicator--comments button" href="#"
+                                           title="Комментарии">
+                                            <svg class="post__indicator-icon" width="19" height="17">
+                                                <use xlink:href="#icon-comment"></use>
+                                            </svg>
+                                            <span>25</span>
+                                            <span class="visually-hidden">количество комментариев</span>
+                                        </a>
+                                    </div>
                                 </div>
-                                <div>
-                                    <ul class="post__tags">
-                                        <?php foreach ($post_tags as $tag): ?>
+                                <ul class="post__tags">
+                                    <?php if (!empty($post['tags'])) : ?>
+                                        <?php foreach ($post['tags'] as $tag): ?>
                                             <li><a href="/search.php?q=<?= $tag ?>">#<?= $tag ?></a></li>
                                         <?php endforeach; ?>
-                                    </ul>
-                                </div>
-
+                                    <?php endif; ?>
+                                </ul>
                             </footer>
                         </article>
                     <?php endforeach; ?>
